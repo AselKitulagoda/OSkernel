29973c9749514bc1e7a1215483d78a078f8b26b4
Asel Kitulagoda <ak17520@my.bristol.ac.uk>
2019-03-21 12:50:08 +0000

    says P3 then resets

--- kernel/hilevel.c
+++ kernel/hilevel.c
@@ -1,16 +1,19 @@
+
 /* Copyright (C) 2017 Daniel Page <csdsp@bristol.ac.uk>
  *
- * Use of this source code is restricted per the CC BY-NC-ND license, a copy of 
- * which can be found via http://creativecommons.org (and should be included as 
+ * Use of this source code is restricted per the CC BY-NC-ND license, a copy of
+ * which can be found via http://creativecommons.org (and should be included as
  * LICENSE.txt within the associated archive or repository).
  */
 
 #include "hilevel.h"
 
-//Coursework 
+
+//Coursework
 
 
 pcb_t pcb[ 4 ]; pcb_t* current = NULL;pcb_t* prev = NULL;pcb_t* next = NULL;
+int processesRunning=0; uint32_t toTos[3]; int currentlyExecuting=0;
 
 
 
@@ -65,12 +68,13 @@ void schedule( ctx_t* ctx ) {
 //             pcb[(i+1)%length].status = STATUS_EXECUTING;
 //             break;
 //         }
-//         
+//
 //     }
-// 
+//
        int arghighestpri = findmax();
        dispatch(ctx,current,&pcb[(arghighestpri)]);
        current->status = STATUS_READY;
+       currentlyExecuting = arghighestpri;
        pcb[arghighestpri].status=STATUS_EXECUTING;
        current->priority = current->initialpriority;
    for (int i =0;i<length;i++){
@@ -85,25 +89,49 @@ void schedule( ctx_t* ctx ) {
 
 
 
-extern void     main_P3(); 
+
+
+extern void     main_P3();
 extern uint32_t tos_P3;
-extern void     main_P4(); 
+extern void     main_P4();
 extern uint32_t tos_P4;
-extern void     main_P5(); 
+extern void     main_P5();
 extern uint32_t tos_P5;
 extern void main_console();
 extern uint32_t tos_console;
 
+uint32_t findtos(int i){
+  uint32_t tos = 0;
+  if (i==2){
+    tos = tos_P4;
+  }
+  else if (i==3){
+    tos = tos_P5;
+  }
+  else if (i==1){
+    tos = tos_P3;
+  }
+  toTos[i]=tos;
+  return tos;
+}
+
+void setpri(int i){
+int priorities[] = {1,5,10};
+int prioritychange[]={4,5,2};
+pcb[i].priority = priorities[i];
+pcb[i].prioritychange=prioritychange[i];
+}
+
 void hilevel_handler_rst(ctx_t* ctx) {
   /* Configure the mechanism for interrupt handling by
    *
-   * - configuring timer st. it raises a (periodic) interrupt for each 
+   * - configuring timer st. it raises a (periodic) interrupt for each
    *   timer tick,
-   * - configuring GIC st. the selected interrupts are forwarded to the 
+   * - configuring GIC st. the selected interrupts are forwarded to the
    *   processor via the IRQ interrupt signal, then
    * - enabling IRQ interrupts.
    */
-  
+
 //   for (int i=0;i<sizeof(pcb_t);i++){
 //       memset(&pcb[i],i,sizeof(pcb_t));
 //       pcb[i].pid = i+3;
@@ -112,7 +140,7 @@ void hilevel_handler_rst(ctx_t* ctx) {
 //       if (i == 0){
 //       pcb[i].ctx.pc= (uint32_t)(&main_P3);
 //       pcb[i].ctx.sp=(uint32_t)(&tos_P3);
- 
+
 //       }
 //       if (i == 1){
 //       pcb[i].ctx.pc= (uint32_t)(&main_P4);
@@ -124,18 +152,20 @@ void hilevel_handler_rst(ctx_t* ctx) {
 //       dispatch(ctx,NULL,&pcb[0]);
 //       }
 //   }
-//  
-//   
+//
+//
   PL011_putc(UART0,'R',true);
   memset( &pcb[ 0 ], 0, sizeof( pcb_t ) );     // initialise 0-th PCB = P_1
-  pcb[ 0 ].pid      = 3;
-  pcb[ 0 ].status   = STATUS_CREATED;
+  pcb[ 0 ].pid      = 0;
+  pcb[ 0 ].status   = STATUS_READY;
   pcb[ 0 ].ctx.cpsr = 0x50;
   pcb[ 0 ].ctx.pc   = ( uint32_t )( &main_console );
   pcb[ 0 ].priority = 20;
   pcb[ 0 ].initialpriority = 20;
   pcb[0].prioritychange = 5;
   pcb[ 0 ].ctx.sp   = ( uint32_t )( &tos_console  );
+
+  toTos[0] = (uint32_t)&tos_console;
 
 //   memset( &pcb[ 1 ], 0, sizeof( pcb_t ) );     // initialise 1-st PCB = P_2
 //   pcb[ 1 ].pid      = 4;
@@ -146,7 +176,7 @@ void hilevel_handler_rst(ctx_t* ctx) {
 //   pcb[1].initialpriority = 20;
 //   pcb[1].prioritychange = 10;
 //   pcb[ 1 ].ctx.sp   = ( uint32_t )( &tos_P4  );
-    
+
 //    memset( &pcb[ 2 ], 0, sizeof( pcb_t ) );     // initialise 1-st PCB = P_2
 //   pcb[ 2 ].pid      = 5;
 //   pcb[ 2 ].status   = STATUS_CREATED;
@@ -156,7 +186,7 @@ void hilevel_handler_rst(ctx_t* ctx) {
 //   pcb[2].initialpriority = 30;
 //   pcb[2].prioritychange = 1;
 //   pcb[ 2 ].ctx.sp   = ( uint32_t )( &tos_P5  );
-    
+
 //   pcb[ 2 ].pid      = 5;
 //   pcb[ 2 ].status   = STATUS_CREATED;
 //   pcb[ 2 ].ctx.cpsr = 0x50;
@@ -165,8 +195,8 @@ void hilevel_handler_rst(ctx_t* ctx) {
 //   pcb[2].initialpriority = 30;
 //   pcb[0].prioritychange = 1;
 //   pcb[ 2 ].ctx.sp   = ( uint32_t )( &tos_console  );
-    
-  
+
+
 
 
   TIMER0->Timer1Load  = 0x00100000; // select period = 2^20 ticks ~= 1 sec
@@ -180,12 +210,13 @@ void hilevel_handler_rst(ctx_t* ctx) {
   GICC0->CTLR         = 0x00000001; // enable GIC interface
   GICD0->CTLR         = 0x00000001; // enable GIC distributor
 
-  /* Once the PCBs are initialised, we arbitrarily select the one in the 0-th 
-   * PCB to be executed: there is no need to preserve the execution context, 
+  /* Once the PCBs are initialised, we arbitrarily select the one in the 0-th
+   * PCB to be executed: there is no need to preserve the execution context,
    * since it is is invalid on reset (i.e., no process will previously have
    * been executing).
    */
-  dispatch( ctx, NULL, &pcb[ findmax() ] );
+  dispatch( ctx, NULL, &pcb[0] );
+  pcb[0].status = STATUS_EXECUTING;
   int_enable_irq();
 
   return;
@@ -213,9 +244,9 @@ void hilevel_handler_irq(ctx_t* ctx) {
 
 
 
-void hilevel_handler_svc( ctx_t* ctx, uint32_t id ) { 
+void hilevel_handler_svc( ctx_t* ctx, uint32_t id ) {
   /* Based on the identifier (i.e., the immediate operand) extracted from the
-   * svc instruction, 
+   * svc instruction,
    *
    * - read  the arguments from preserved usr mode registers,
    * - perform whatever is appropriate for this system call, then
@@ -230,22 +261,48 @@ void hilevel_handler_svc( ctx_t* ctx, uint32_t id ) {
     }
 
     case 0x01 : { // 0x01 => write( fd, x, n )
-      int   fd = ( int   )( ctx->gpr[ 0 ] );  
-      char*  x = ( char* )( ctx->gpr[ 1 ] );  
-      int    n = ( int   )( ctx->gpr[ 2 ] ); 
+      int   fd = ( int   )( ctx->gpr[ 0 ] );
+      char*  x = ( char* )( ctx->gpr[ 1 ] );
+      int    n = ( int   )( ctx->gpr[ 2 ] );
 
       for( int i = 0; i < n; i++ ) {
         PL011_putc( UART0, *x++, true );
       }
-      
+
       ctx->gpr[ 0 ] = n;
 
       break;
+    }
+    case 0x03:{
+      processesRunning +=1;
+      memset(&pcb[processesRunning],0,sizeof(pcb_t));
+      memcpy(&pcb[processesRunning].ctx,ctx,sizeof(ctx_t));
+      pcb[processesRunning].pid = processesRunning;
+      pcb[processesRunning].status = pcb[currentlyExecuting].status;
+      pcb[processesRunning].ctx.cpsr = ctx->cpsr; //new
+      toTos[processesRunning] = (uint32_t) findtos(processesRunning);
+      uint32_t offset = (uint32_t) findtos(currentlyExecuting) - ctx->sp;
+      memcpy((void*)(toTos[processesRunning]-offset),(void*)(toTos[currentlyExecuting]-offset),offset);
+      pcb[processesRunning].ctx.sp = (uint32_t) toTos[processesRunning]-offset;
+      setpri(processesRunning);
+      pcb[processesRunning].ctx.gpr[0]=0;
+      ctx->gpr[0] = processesRunning;
+      break;
+
+
     }
       case 0x04:{
           current->status = STATUS_TERMINATED;
           schedule(ctx);
           break;
+      }
+
+      case 0x05:{
+
+        void* execPid = (void*) ctx->gpr[0];
+        ctx->sp = toTos[currentlyExecuting];
+        ctx->pc = (uint32_t) execPid;
+        break;
       }
 
     default   : { // 0x?? => unknown/unsupported
